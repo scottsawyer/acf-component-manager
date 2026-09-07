0ae65f7ed249924f4b1cd0446d40755c5f6c3b41
Scott Sawyer <scott@scottsawyerconsulting.com>
2026-09-07 01:23:11 -0400

    Tests

--- tests/php/unit/test-component-manager.php
+++ tests/php/unit/test-component-manager.php
@@ -2,22 +2,26 @@
 
 use PHPUnit\Framework\TestCase;
 use AcfComponentManager\Controller\ComponentManager;
+use AcfComponentManager\Service\ComponentService;
 
 class TestComponentManager extends TestCase {
 	protected $componentManager;
 
+	protected $componentService;
+
 	protected function setUp(): void {
 		$this->componentManager = new ComponentManager();
+		$this->componentService = new ComponentService();
 	}
 
-	public function testGetThemeComponents() {
-		$themeComponents = $this->componentManager->get_theme_components();
-		// Assert that $themeComponents is an array or contains the expected data.
-		$this->assertIsArray($themeComponents);
+	public function testGetDiscoveredComponents() {
+		$discoveredComponents = $this->componentService->get_discovered_components();
+		// Assert $discoveredComponents is an array or contains the expected data.
+		$this->assertIsArray($discoveredComponents);
 	}
 
 	public function testGetStoredComponents() {
-		$storedComponents = $this->componentManager->get_stored_components();
+		$storedComponents = $this->componentService->get_stored_components();
 		// Assert that $storedComponents is an array or contains the expected data.
 		$this->assertIsArray($storedComponents);
 	}
